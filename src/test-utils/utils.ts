0d566949693ec16f4fa0d268208a82f7e6c07a08
Roy Avidan <royavi1403@gmail.com>
2026-05-10 21:52:11 +0300

    arranged tests better

--- src/test-utils/utils.ts
+++ src/test-utils/utils.ts
@@ -2,9 +2,9 @@ import * as pdfjs from 'pdfjs-dist/legacy/build/pdf.js'
 import { promises as fs } from 'fs'
 import path from 'path'
 
-export const readJSONFile = async (i: number, prefix: string = '') => JSON.parse(await fs.readFile(path.join(__dirname, 'data', `${prefix}${i}.json`), 'utf-8'))
+export const readJSONFile = async (i: number, name: string) => JSON.parse(await fs.readFile(path.join(__dirname, 'data', `test${i}`, `${name}.json`), 'utf-8'))
 export const readPDFFile = async (i: number) => {
-    const fileData = await fs.readFile(path.join(__dirname, 'data', `${i}.pdf`))
+    const fileData = await fs.readFile(path.join(__dirname, 'data', `test${i}`, `doc.pdf`))
     const loadingTask = pdfjs.getDocument({ data: fileData.buffer })
     const pdfDocument = await loadingTask.promise
     return pdfDocument
